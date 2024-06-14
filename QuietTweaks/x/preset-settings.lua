107616a02053d6f00dea05ede245e8b59c586298
tanghao <tanghao.hb@gmail.com>
2024-06-14 22:00:40 +0800

    merge unitframe/reposition into preset-settings

--- QuietTweaks/x/preset-settings.lua
+++ QuietTweaks/x/preset-settings.lua
@@ -1,4 +1,25 @@
 (function()
     SetCVar("profanityFilter", "0");
     SetCVar("cameraDistanceMax", 50);
+
+    local function repositionPlayerFrame()
+        PlayerFrame:ClearAllPoints();
+        PlayerFrame:SetPoint("TOPRIGHT", UIParent, "CENTER", -40, -120);
+    end
+
+    local function repositionTargetFrame()
+        TargetFrame:ClearAllPoints();
+        TargetFrame:SetPoint("TOPLEFT", UIParent, "CENTER", 40, -120);
+    end
+
+    (function()
+        local f = CreateFrame("Frame");
+        f:RegisterEvent("PLAYER_ENTERING_WORLD");
+        f:SetScript("OnEvent", function()
+            f:UnregisterAllEvents();
+            f:Hide();
+            repositionPlayerFrame();
+            repositionTargetFrame();
+        end);
+    end)();
 end)();
