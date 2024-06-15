8c82417247ae76b3ad679a99976e8e69302fcc91
tanghao <tanghao.hb@gmail.com>
2024-06-15 21:31:57 +0800

    always show MultiBarBottomLeft and MultiBarBottomRight

--- QuietTweaks/x/preset-settings.lua
+++ QuietTweaks/x/preset-settings.lua
@@ -19,6 +19,18 @@
         end
     end
 
+    local function showMultiActionBars()
+        SHOW_MULTI_ACTIONBAR_1 = 1;
+        SHOW_MULTI_ACTIONBAR_2 = 1;
+        SHOW_MULTI_ACTIONBAR_3 = nil;
+        SHOW_MULTI_ACTIONBAR_4 = nil;
+        MultiActionBar_Update();
+        UIParent_ManageFramePositions();
+
+        ALWAYS_SHOW_MULTIBARS = 1;
+        MultiActionBar_UpdateGridVisibility();
+    end
+
     (function()
         local f = CreateFrame("Frame");
         f:RegisterEvent("PLAYER_ENTERING_WORLD");
@@ -28,6 +40,7 @@
             repositionPlayerFrame();
             repositionTargetFrame();
             repositionChatFrame1();
+            showMultiActionBars();
         end);
     end)();
 end)();
