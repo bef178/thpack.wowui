5d3d66b586dc05dead7c31ee92f4585ce58d4f7b
tanghao <tanghao.hb@gmail.com>
2025-01-02 19:35:39 +0800

    refine flight poi

---
+++ QuietTweaks/lib/db-npc-flightmaster-turtlewow.lua
@@ -0,0 +1,858 @@
+local flightMasters = {
+    {
+        id = 352,
+        name = "Dungar Longdrink",
+        title = "Gryphon Master",
+        locations = {
+            ["Stormwind City"] = {
+                x = 0.710,
+                y = 0.725,
+            },
+            ["Stormwind, Elwynn"] = {
+                x = 0.433,
+                y = 0.328,
+            },
+        },
+    },
+    {
+        id = 931,
+        name = "Ariena Stormfeather",
+        title = "Gryphon Master",
+        locations = {
+            ["Redridge Mountains"] = {
+                x = 0.306,
+                y = 0.596,
+            },
+            ["Lakeshire, Redridge"] = {
+                x = 0.557,
+                y = 0.301,
+            },
+        },
+    },
+    {
+        id = 1387,
+        name = "Thysta",
+        title = "Wind Rider Master",
+        locations = {
+            ["Stranglethorn Vale"] = {
+                x = 0.326,
+                y = 0.292,
+            },
+        },
+    },
+    {
+        id = 1571,
+        name = "Shellei Brondir",
+        title = "Gryphon Master",
+        locations = {
+            ["Wetlands"] = {
+                x = 0.096,
+                y = 0.596,
+            },
+            ["Menethil Harbor, Wetlands"] = {
+                x = 0.491,
+                y = 0.559,
+            },
+        },
+    },
+    {
+        id = 1572,
+        name = "Thorgrum Borrelson",
+        title = "Gryphon Master",
+        locations = {
+            ["Loch Modan"] = {
+                x = 0.338,
+                y = 0.508,
+            },
+            ["Thelsamar, Loch Modan"] = {
+                x = 0.589,
+                y = 0.484,
+            },
+        },
+    },
+    {
+        id = 1573,
+        name = "Gryth Thurden",
+        title = "Gryphon Master",
+        locations = {
+            ["Ironforge"] = {
+                x = 0.556,
+                y = 0.480,
+            },
+            ["Ironforge, Dun Morogh"] = {
+                x = 0.508,
+                y = 0.512,
+            },
+        },
+    },
+    {
+        id = 2226,
+        name = "Karos Razok",
+        title = "Bat Handler",
+        locations = {
+            ["Silverpine Forest"] = {
+                x = 0.456,
+                y = 0.426,
+            },
+        },
+    },
+    {
+        id = 2299,
+        name = "Borgus Stoutarm",
+        title = "Gryphon Master",
+        locations = {
+            ["Burning Steppes"] = {
+                x = 0.844,
+                y = 0.682,
+            },
+            ["Morgan's Vigil, Burning Steppes"] = {
+                x = 0.581,
+                y = 0.349,
+            },
+        },
+    },
+    {
+        id = 2389,
+        name = "Zarise",
+        title = "Bat Handler",
+        locations = {
+            ["Hillsbrad Foothills"] = {
+                x = 0.602,
+                y = 0.186,
+            },
+        },
+    },
+    {
+        id = 2409,
+        name = "Felicia Maline",
+        title = "Gryphon Master",
+        locations = {
+            ["Duskwood"] = {
+                x = 0.776,
+                y = 0.444,
+            },
+            ["Darkshire, Duskwood"] = {
+                x = 0.513,
+                y = 0.251,
+            },
+        },
+    },
+    {
+        id = 2432,
+        name = "Darla Harris",
+        title = "Gryphon Master",
+        locations = {
+            ["Hillsbrad Foothills"] = {
+                x = 0.494,
+                y = 0.526,
+            },
+            ["Southshore, Hillsbrad"] = {
+                x = 0.479,
+                y = 0.700,
+            },
+        },
+    },
+    {
+        id = 2835,
+        name = "Cedrik Prose",
+        title = "Gryphon Master",
+        locations = {
+            ["Eastern Kingdoms"] = {
+                x = 0.525999,
+                y = 0.371044,
+            },
+            ["Arathi Highlands"] = {
+                x = 0.456,
+                y = 0.460,
+            },
+            ["Refuge Pointe, Arathi"] = {
+                x = 0.570,
+                y = 0.676,
+            },
+        },
+    },
+    {
+        id = 2851,
+        name = "Urda",
+        title = "Wind Rider Master",
+        locations = {
+            ["Winterspring"] = {
+                x = 0.730,
+                y = 0.326,
+            },
+        },
+    },
+    {
+        id = 2858,
+        name = "Gringer",
+        title = "Wind Rider Master",
+        locations = {
+            ["Stranglethorn Vale"] = {
+                x = 0.268,
+                y = 0.770,
+            },
+        },
+    },
+    {
+        id = 2859,
+        name = "Gyll",
+        title = "Gryphon Master",
+        locations = {
+            ["Stranglethorn Vale"] = {
+                x = 0.276,
+                y = 0.776,
+            },
+            ["Booty Bay, Stranglethorn"] = {
+                x = 0.434,
+                y = 0.069,
+            },
+        },
+    },
+    {
+        id = 2861,
+        name = "Gorrik",
+        title = "Wind Rider Master",
+        locations = {
+            ["Badlands"] = {
+                x = 0.040,
+                y = 0.448,
+            },
+        },
+    },
+    {
+        id = 2941,
+        name = "Lanie Reed",
+        title = "Gryphon Master",
+        locations = {
+            ["Searing Gorge"] = {
+                x = 0.378,
+                y = 0.306,
+            },
+        },
+    },
+    {
+        id = 2995,
+        name = "Tal",
+        title = "Wind Rider Master",
+        locations = {
+            ["Thunder Bluff"] = {
+                x = 0.468,
+                y = 0.500,
+            },
+        },
+    },
+    {
+        id = 3305,
+        name = "Grisha",
+        title = "Wind Rider Master",
+        locations = {
+            ["Searing Gorge"] = {
+                x = 0.348,
+                y = 0.308,
+            },
+        },
+    },
+    {
+        id = 3310,
+        name = "Doras",
+        title = "Wind Rider Master",
+        locations = {
+            ["Orgrimmar"] = {
+                x = 0.452,
+                y = 0.638,
+            },
+        },
+    },
+    {
+        id = 3615,
+        name = "Devrak",
+        title = "Wind Rider Master",
+        locations = {
+            ["The Barrens"] = {
+                x = 0.515,
+                y = 0.303,
+            },
+        },
+    },
+    {
+        id = 3838,
+        name = "Vesprystus",
+        title = "Hippogryph Master",
+        locations = {
+            ["Teldrassil"] = {
+                x = 0.584,
+                y = 0.940,
+            },
+            ["Rut'theran Village, Teldrassil"] = {
+                x = 0.416,
+                y = 0.843,
+            },
+        },
+    },
+    {
+        id = 3841,
+        name = "Caylais Moonfeather",
+        title = "Hippogryph Master",
+        locations = {
+            ["Darkshore"] = {
+                x = 0.364,
+                y = 0.456,
+            },
+            ["Auberdine, Darkshore"] = {
+                x = 0.428,
+                y = 0.748,
+            },
+        },
+    },
+    {
+        id = 4267,
+        name = "Daelyshia",
+        title = "Hippogryph Master",
+        locations = {
+            ["Ashenvale"] = {
+                x = 0.344,
+                y = 0.480,
+            },
+            ["Astranaar, Ashenvale"] = {
+                x = 0.463,
+                y = 0.604,
+            },
+        },
+    },
+    {
+        id = 4312,
+        name = "Tharm",
+        title = "Wind Rider Master",
+        locations = {
+            ["Stonetalon Mountains"] = {
+                x = 0.452,
+                y = 0.598,
+            },
+        },
+    },
+    {
+        id = 4314,
+        name = "Gorkas",
+        title = "Wind Rider Master",
+        locations = {
+            ["The Hinterlands"] = {
+                x = 0.816,
+                y = 0.818,
+            },
+        },
+    },
+    {
+        id = 4317,
+        name = "Nyse",
+        title = "Wind Rider Master",
+        locations = {
+            ["Thousand Needles"] = {
+                x = 0.450,
+                y = 0.492,
+            },
+        },
+    },
+    {
+        id = 4319,
+        name = "Thyssiana",
+        title = "Hippogryph Master",
+        locations = {
+            ["Feralas"] = {
+                x = 0.895,
+                y = 0.459,
+            },
+            ["Thousand Needles"] = {
+                x = 0.780,
+                y = 0.179,
+            },
+        },
+    },
+    {
+        id = 4321,
+        name = "Baldruc",
+        title = "Gryphon Master",
+        locations = {
+            ["Dustwallow Marsh"] = {
+                x = 0.676,
+                y = 0.514,
+            },
+            ["Theramore, Dustwallow Marsh"] = {
+                x = 0.636,
+                y = 0.331,
+            },
+        },
+    },
+    {
+        id = 4407,
+        name = "Teloren",
+        title = "Hippogryph Master",
+        locations = {
+            ["Stonetalon Mountains"] = {
+                x = 0.364,
+                y = 0.072,
+            },
+        },
+    },
+    {
+        id = 4551,
+        name = "Michael Garrett",
+        title = "Bat Handler",
+        locations = {
+            ["Undercity"] = {
+                x = 0.636,
+                y = 0.486,
+            },
+        },
+    },
+    {
+        id = 6026,
+        name = "Breyk",
+        title = "Wind Rider Master",
+        locations = {
+            ["Swamp of Sorrows"] = {
+                x = 0.460,
+                y = 0.546,
+            },
+        },
+    },
+    {
+        id = 6706,
+        name = "Baritanas Skyriver",
+        title = "Hippogryph Master",
+        locations = {
+            ["Desolace"] = {
+                x = 0.646,
+                y = 0.106,
+            },
+            ["Nijel's Point, Desolace"] = {
+                x = 0.396,
+                y = 0.493,
+            },
+        },
+    },
+    {
+        id = 6726,
+        name = "Thalon",
+        title = "Wind Rider Master",
+        locations = {
+            ["Desolace"] = {
+                x = 0.216,
+                y = 0.740,
+            },
+        },
+    },
+    {
+        id = 7823,
+        name = "Bera Stonehammer",
+        title = "Gryphon Master",
+        locations = {
+            ["Tanaris"] = {
+                x = 0.510,
+                y = 0.292,
+            },
+            ["Gadgetzan, Tanaris"] = {
+                x = 0.604,
+                y = 0.191,
+            },
+        },
+    },
+    {
+        id = 7824,
+        name = "Bulkrek Ragefist",
+        title = "Wind Rider Master",
+        locations = {
+            ["Tanaris"] = {
+                x = 0.516,
+                y = 0.256,
+            },
+        },
+    },
+    {
+        id = 8018,
+        name = "Guthrum Thunderfist",
+        title = "Gryphon Master",
+        locations = {
+            ["The Hinterlands"] = {
+                x = 0.110,
+                y = 0.460,
+            },
+            ["Aerie Peak, The Hinterlands"] = {
+                x = 0.547,
+                y = 0.746,
+            },
+        },
+    },
+    {
+        id = 8019,
+        name = "Fyldren Moonfeather",
+        title = "Hippogryph Master",
+        locations = {
+            ["Feralas"] = {
+                x = 0.302,
+                y = 0.432,
+            },
+            ["Feathermoon, Feralas"] = {
+                x = 0.314,
+                y = 0.308,
+            },
+        },
+    },
+    {
+        id = 8020,
+        name = "Shyn",
+        title = "Wind Rider Master",
+        locations = {
+            ["Feralas"] = {
+                x = 0.754,
+                y = 0.442,
+            },
+        },
+    },
+    {
+        id = 8609,
+        name = "Alexandra Constantine",
+        title = "Gryphon Master",
+        locations = {
+            ["Blasted Lands"] = {
+                x = 0.656,
+                y = 0.244,
+            },
+            ["Nethergarde Keep, Blasted Lands"] = {
+                x = 0.613,
+                y = 0.223,
+            },
+        },
+    },
+    {
+        id = 8610,
+        name = "Kroum",
+        title = "Wind Rider Master",
+        locations = {
+            ["Azshara"] = {
+                x = 0.220,
+                y = 0.496,
+            },
+        },
+    },
+    {
+        id = 10378,
+        name = "Omusa Thunderhorn",
+        title = "Wind Rider Master",
+        locations = {
+            ["The Barrens"] = {
+                x = 0.444,
+                y = 0.590,
+            },
+        },
+    },
+    {
+        id = 10583,
+        name = "Gryfe",
+        title = "Flight Master",
+        locations = {
+            ["Un'Goro Crater"] = {
+                x = 0.452,
+                y = 0.058,
+            },
+            ["Marshal's Refuge, Un'Goro Crater"] = {
+                x = 0.498,
+                y = 0.236,
+            },
+        },
+    },
+    {
+        id = 10897,
+        name = "Sindrayl",
+        title = "Hippogryph Master",
+        locations = {
+            ["Moonglade"] = {
+                x = 0.480,
+                y = 0.672,
+            },
+        },
+    },
+    {
+        id = 11138,
+        name = "Maethrya",
+        title = "Hippogryph Master",
+        locations = {
+            ["Winterspring"] = {
+                x = 0.622,
+                y = 0.366,
+            },
+        },
+    },
+    {
+        id = 11139,
+        name = "Yugrek",
+        title = "Wind Rider Master",
+        locations = {
+            ["Winterspring"] = {
+                x = 0.604,
+                y = 0.364,
+            },
+        },
+    },
+    {
+        id = 11899,
+        name = "Shardi",
+        title = "Wind Rider Master",
+        locations = {
+            ["Dustwallow Marsh"] = {
+                x = 0.356,
+                y = 0.318,
+            },
+        },
+    },
+    {
+        id = 11900,
+        name = "Brakkar",
+        title = "Wind Rider Master",
+        locations = {
+            ["Felwood"] = {
+                x = 0.344,
+                y = 0.538,
+            },
+        },
+    },
+    {
+        id = 11901,
+        name = "Andruk",
+        title = "Wind Rider Master",
+        locations = {
+            ["Ashenvale"] = {
+                x = 0.122,
+                y = 0.338,
+            },
+        },
+    },
+    {
+        id = 12577,
+        name = "Jarrodenus",
+        title = "Hippogryph Master",
+        locations = {
+            ["Azshara"] = {
+                x = 0.118,
+                y = 0.776,
+            },
+        },
+    },
+    {
+        id = 12578,
+        name = "Mishellena",
+        title = "Hippogryph Master",
+        locations = {
+            ["Felwood"] = {
+                x = 0.624,
+                y = 0.242,
+            },
+        },
+    },
+    {
+        id = 12596,
+        name = "Bibilfaz Featherwhistle",
+        title = "Gryphon Master",
+        locations = {
+            ["Western Plaguelands"] = {
+                x = 0.428,
+                y = 0.850,
+            },
+            ["Chillwind Camp, Western Plaguelands"] = {
+                x = 0.521,
+                y = 0.776,
+            },
+        },
+    },
+    {
+        id = 12616,
+        name = "Vhulgra",
+        title = "Wind Rider Master",
+        locations = {
+            ["Ashenvale"] = {
+                x = 0.732,
+                y = 0.616,
+            },
+        },
+    },
+    {
+        id = 12617,
+        name = "Khaelyn Steelwing",
+        title = "Gryphon Master",
+        locations = {
+            ["Eastern Plaguelands"] = {
+                x = 0.816,
+                y = 0.592,
+            },
+            ["Light's Hope Chapel, Eastern Plaguelands"] = {
+                x = 0.700,
+                y = 0.837,
+            },
+        },
+    },
+    {
+        id = 12636,
+        name = "Georgia",
+        title = "Bat Handler",
+        locations = {
+            ["Eastern Plaguelands"] = {
+                x = 0.802,
+                y = 0.570,
+            },
+        },
+    },
+    {
+        id = 12740,
+        name = "Faustron",
+        title = "Wind Rider Master",
+        locations = {
+            ["Moonglade"] = {
+                x = 0.322,
+                y = 0.666,
+            },
+        },
+    },
+    {
+        id = 13177,
+        name = "Vahgruk",
+        title = "Wind Rider Master",
+        locations = {
+            ["Burning Steppes"] = {
+                x = 0.656,
+                y = 0.242,
+            },
+        },
+    },
+    {
+        id = 15177,
+        name = "Cloud Skydancer",
+        title = "Hippogryph Master",
+        locations = {
+            ["Silithus"] = {
+                x = 0.506,
+                y = 0.346,
+            },
+        },
+    },
+    {
+        id = 15178,
+        name = "Runk Windtamer",
+        title = "Wind Rider Master",
+        locations = {
+            ["Silithus"] = {
+                x = 0.488,
+                y = 0.366,
+            },
+        },
+    },
+    {
+        id = 16227,
+        name = "Bragok",
+        title = "Flight Master",
+        locations = {
+            ["The Barrens"] = {
+                x = 0.630,
+                y = 0.372,
+            },
+            ["Ratchet, The Barrens"] = {
+                x = 0.606,
+                y = 0.451,
+            },
+        },
+    },
+    {
+        id = 52093,
+        name = "Falok Thurden",
+        title = "Gryphon Master",
+        locations = {
+            ["Dun Morogh"] = {
+                x = 0.686,
+                y = 0.187,
+            },
+            ["Ironforge Airfields, Dun Morogh"] = {
+                x = 0.528,
+                y = 0.527,
+            },
+        },
+    },
+    {
+        id = 52094,
+        name = "Greta Stonehammer",
+        title = "Gryphon Master",
+        locations = {
+            ["Wetlands"] = {
+                x = 0.227,
+                y = 0.653,
+            },
+            ["Dun Agrath, Wetlands"] = {
+                x = 0.516,
+                y = 0.552,
+            },
+        },
+    },
+    {
+        id = 61532,
+        name = "Levenda Skytalon",
+        title = "Hippogryph Master",
+        locations = {
+            ["Winterspring"] = {
+                x = 0.475,
+                y = 0.641,
+            },
+        },
+    },
+    {
+        id = 61623,
+        name = "Orrik Thunderbeard",
+        title = "Gryphon Master",
+        locations = {
+            ["The Barrens"] = {
+                x = 0.341,
+                y = 0.109,
+            },
+        },
+    },
+};
+
+local A = A;
+
+for _, npc in ipairs(flightMasters) do
+    A.db.npcs[npc.id] = npc;
+    A.db.flightMasterIds[npc.id] = 1;
+end
+flightMasters = nil;
+
+
+local TAXI_NODES_MEMORY = {
+	[1] = {
+		["Tel Co. Basecamp, Tel'Abim"] = {
+			["y"] = 0.122,
+			["x"] = 0.716,
+			["continentIndex"] = 1,
+			["reachable"] = true,
+			["name"] = "Tel Co. Basecamp, Tel'Abim",
+		},
+	},
+	[2] = {
+		["Alah'Thalas"] = {
+			["y"] = 0.9310000000000001,
+			["x"] = 0.584,
+			["continentIndex"] = 2,
+			["reachable"] = true,
+			["name"] = "Alah'Thalas",
+		},
+		["Caelan's Rest, Lapidis Isle"] = {
+			["y"] = 0.189,
+			["x"] = 0.309,
+			["continentIndex"] = 2,
+			["reachable"] = true,
+			["name"] = "Caelan's Rest, Lapidis Isle",
+		},
+		["Ravenshire, Gilneas"] = {
+			["y"] = 0.634,
+			["x"] = 0.41,
+			["continentIndex"] = 2,
+			["reachable"] = true,
+			["name"] = "Ravenshire, Gilneas",
+		},
+	},
+}
