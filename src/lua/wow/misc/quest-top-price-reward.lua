023f67ca8d7a242cb72f11d1145265fc51d75696
tanghao <tanghao.hb@gmail.com>
2026-05-04 17:53:24 +0800

    add quest-top-price-reward

---
+++ src/lua/wow/misc/quest-top-price-reward.lua
@@ -0,0 +1,23 @@
+-- 自动选择最贵的任务奖励
+local f = CreateFrame("Frame")
+f:RegisterEvent("QUEST_COMPLETE")
+f:SetScript("OnEvent", function()
+    local topPrice = 0
+    local topPriceIndex = 0
+    for i = 1, GetNumQuestChoices() do
+        local itemLink = GetQuestItemLink("choice", i)
+        if itemLink then
+            local itemId = ItemUtil.parseItemLink(itemLink)
+            if itemId then
+                local price = ItemUtil.getItem(itemLink).itemRecyclePrice
+                if price and price > topPrice then
+                    topPrice = price
+                    topPriceIndex = i
+                end
+            end
+        end
+    end
+    if topPriceIndex > 0 then
+        _G["QuestInfoRewardsFrameQuestInfoItem" .. topPriceIndex]:Click()
+    end
+end)
